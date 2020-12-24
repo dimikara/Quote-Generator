2df4db1f47d1cbcaccd515f6b6782f09da5668d4
Dimitra Karamperi <dkaramperi@yahoo.gr>
2020-12-24 21:36:15 +0200

    chore: Add more quotes

--- js/javascript.js
+++ js/javascript.js
@@ -595,6 +595,14 @@ const myQuotes = [
     {
         "quote": "You are the books you read, the films you watch, the music you listen to, the people you spend time with the conversation you engage in. Choose wisely what you feed your mind.",
         "author": "Jac Vanek"
+    },
+    {
+        "quote": "Ability is nothing without opportunity.",
+        "author": "Napoleon Bonaparte"
+    },
+    {
+        "quote": "Sometimes you face difficulties not because you're doing something wrong, but because you're doing something right.",
+        "author": "Unkonown"
     }
 ];
 
